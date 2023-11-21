8bce885e850e3ae5f08474b00bf4a02a9c42243f
markofin0 <m.marullo25@ncf.edu>
2023-11-21 17:34:09 -0500

    icon fix

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ screen = pygame.display.set_mode((800, 600))
 
 # Title and Icon
 pygame.display.set_caption("Heart of the Cards")
+icon = pygame.image.load('icon.png')
+pygame.display.set_icon(icon)
 
 # Game Loop
 running = True
