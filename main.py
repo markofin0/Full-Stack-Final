d586d949a17352174bd8c2bb587c695df44e2736
markofin0 <m.marullo25@ncf.edu>
2023-11-29 14:39:50 -0500

    little rewrite

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 import pygame
 
 # initializes pygame
@@ -6,14 +8,20 @@ pygame.init()
 # creates view
 screen = pygame.display.set_mode((800, 600))
 
+clock = pygame.time.Clock()
+
+
 # Title and Icon
 pygame.display.set_caption("Heart of the Cards")
 icon = pygame.image.load('icon.png')
 pygame.display.set_icon(icon)
 
 # Game Loop
-running = True
-while running:
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    clock.tick(60)
