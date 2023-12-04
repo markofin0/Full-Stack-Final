52e65d49a53406510cee9c2fe1deaeb5dd3af118
markofin0 <m.marullo25@ncf.edu>
2023-12-04 16:57:22 -0500

    finish game object

--- main.py
+++ main.py
@@ -1,27 +1,71 @@
-import sys
-
 import pygame
+import sys
+from sprites import *
+from config import *
+
 
 # initializes pygame
-pygame.init()
+
 
 # creates view
-screen = pygame.display.set_mode((800, 600))
+class Game:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption("Heart of the Cards")
+        self.icon = pygame.image.load('icon.png')
+        self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+        self.clock = pygame.time.Clock()
+        pygame.display.set_icon(self.icon)
+        self.running = True
 
-clock = pygame.time.Clock()
+    def new(self):
+        self.playing = True
+        self.all_sprites = pygame.sprite.LayeredUpdates()
+        self.blocks = pygame.sprite.LayeredUpdates()
+        self.enemies = pygame.sprite.LayeredUpdates()
+        self.attacks = pygame.sprite.LayeredUpdates()
+
+        self.player = Player(self, 1, 2)
+
+    def events(self):
+        # game loop events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.playing = False
+                self.running = False
+
+    def update(self):
+        # game loop updates
+        self.all_sprites.update()
+
+    def draw(self):
+        self.screen.fill(BLACK)
+        self.all_sprites.draw(self.screen)
+        self.clock.tick(FPS)
+        pygame.display.update()
+
+    def main(self):
+        # game loop
+        while self.playing:
+            self.events()
+            self.update()
+            self.draw()
+        self.running = False
+
+    def game_over(self):
+        pass
+
+    def intro_screen(self):
+        pass
 
 
-# Title and Icon
-pygame.display.set_caption("Heart of the Cards")
-icon = pygame.image.load('icon.png')
-pygame.display.set_icon(icon)
+g = Game()
+g.intro_screen()
+g.new()
+while g.running:
+    g.main()
+    g.game_over()
 
-# Game Loop
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+pygame.quit()
+sys.exit()
 
-    pygame.display.update()
-    clock.tick(60)
