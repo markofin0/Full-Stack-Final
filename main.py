6498687036db4be8979dcba96f4d99016c8189b7
markofin0 <m.marullo25@ncf.edu>
2023-12-11 16:21:54 -0500

    intro + game over screens added

--- main.py
+++ main.py
@@ -17,10 +17,13 @@ class Game:
         self.clock = pygame.time.Clock()
         pygame.display.set_icon(self.icon)
         self.running = True
+        self.font = pygame.font.Font('arial.ttf', 32)
 
         self.character_spritesheet = Spritesheet('img/character.png')
         self.terrain_spritesheet = Spritesheet('img/terrain.png')
         self.enemy_spritesheet = Spritesheet('img/enemy.png')
+        self.intro_background = pygame.image.load('./img/introbackground.png')
+        self.go_background = pygame.image.load('./img/gameover.png')
 
     def createTilemap(self):
         for i, row in enumerate(tilemap):
@@ -68,13 +71,59 @@ class Game:
             self.update()
             self.draw()
             await asyncio.sleep(0)
-        self.running = False
 
     def game_over(self):
-        pass
+        text = self.font.render("GAME OVER", True, WHITE)
+        text_rect = text.get_rect(center=(WIN_WIDTH/2, WIN_HEIGHT/2))
+
+        restart_button = Button(10, WIN_HEIGHT - 60, 120, 50, WHITE, BLACK, 'Restart', 32)
+
+        for sprite in self.all_sprites:
+            sprite.kill()
+
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+                mouse_pos = pygame.mouse.get_pos()
+                mouse_pressed = pygame.mouse.get_pressed()
+
+                if restart_button.is_pressed(mouse_pos, mouse_pressed):
+                    self.new()
+                    asyncio.run(self.main())
+
+                self.screen.blit(self.go_background, (0,0))
+                self.screen.blit(text, text_rect)
+                self.screen.blit(restart_button.image, restart_button.rect)
+                self.clock.tick(FPS)
+                pygame.display.update()
 
     def intro_screen(self):
-        pass
+        intro = True
+
+        title = self.font.render('Swag Money Game', True, BLACK)
+        title_rect = title.get_rect(x=10,y=10)
+
+        play_button = Button(10, 50, 100, 50, WHITE, BLACK, 'Play', 32)
+
+        while intro:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    intro = False
+                    self.running = False
+
+            mouse_pos = pygame.mouse.get_pos()
+            mouse_pressed = pygame.mouse.get_pressed()
+
+            if play_button.is_pressed(mouse_pos, mouse_pressed):
+                intro = False
+
+            self.screen.blit(self.intro_background, (0,0))
+            self.screen.blit(title, title_rect)
+            self.screen.blit(play_button.image, play_button.rect)
+            self.clock.tick(FPS)
+            pygame.display.update()
 
 
 g = Game()
