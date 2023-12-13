c16c9eff92682df0fa01737edeb238170e939546
markofin0 <m.marullo25@ncf.edu>
2023-12-12 20:24:07 -0500

    had to split web app and python project into two

--- app.py
+++ app.py
@@ -21,12 +21,24 @@ def get_db_connection():
 
 @app.route('/', methods=['GET', 'POST'])
 def main_page():
+    session['authenticated'] = False
     return render_template('test.html')
 
 
 @app.route('/addingsoon', methods=['GET', 'POST'])
 def addingsoon_page():
     return render_template('addingsoon.html')
+
+
+@app.route('/snake', methods=['GET', 'POST'])
+def snake():
+    return render_template('snake.html')
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    session['authenticated'] = False
+    return render_template('logout.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -39,13 +51,43 @@ def login():
 
         if cur.fetchone() is None:
             con.close()
-            return render_template('index.html', message='Invalid credentials')
+            return render_template('index.html', error_message='Invalid credentials')
         else:
             con.close()
             session['authenticated'] = True
             session['name'] = request.form['username']
-            return redirect(url_for('index'))  # Redirect to the main page after login
-    return render_template('index.html')
+            return render_template('test.html')  # Redirect to the main page after login
+    return render_template('index.html', error_message=None)
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        # Check if the email already exists in the database
+        cursor.execute('SELECT * FROM users WHERE email = ?', (email,))
+        user = cursor.fetchone()
+
+        if user:
+            # Email already exists, return an error
+            error_message = 'Email already exists. Please choose a different email.'
+            conn.close()
+            return render_template('signup.html', error_message=error_message)
+
+        # Email does not exist, insert the new user into the database
+        cursor.execute('INSERT INTO users (email, password) VALUES (?, ?)', (email, password))
+        conn.commit()
+        conn.close()
+
+        # Redirect to a success page or login page
+        return redirect(url_for('login'))
+
+    return render_template('signup.html', error_message=None)
 
 
 if __name__ == '__main__':
